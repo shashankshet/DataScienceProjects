e25df9bd45d56b03e8328c6829e76a884987232d
shashankshet <48438739+shashankshet@users.noreply.github.com>
2020-08-07 16:59:39 +0530

    Add files via upload

---
+++ resort_management_system-master/index.php
@@ -0,0 +1,38 @@
+<?php
+include "include/header.php";
+include "connect.php";
+ ?>
+ <link rel="stylesheet" type="text/css" href="style.css">
+
+<center><h1>PARADISE ROYALE</h1>
+   <center><img src="img/Resort.jpg" id="himg">
+
+<br><br><br>
+
+
+
+
+      <div id="hdiscrp">
+         <article>
+            <h2><center>Description</center></h2>
+            A short ride from Tagbilaran Airport, discover a paradise hideaway where luxurious seclusion meets exotic natural beauty.
+            This verdant, beach-fringed island offers sophisticated pleasures on land
+            and water - from genuine Filipino therapies in the spa to splashing,
+            sailing or diving in the jewel-like Mindanao Sea and Bohol's most pristine lagoon.
+        </article>
+      </div>
+
+</div>
+
+
+<div id="hbooknow">
+   <center>
+      <a href="room.php">Rooms</a>
+      <a href="booking.php">Book Now</a>
+   </center>
+</div>
+
+
+<?php
+include "include/footer.php";
+ ?>
